86b9b12b21964b8a72f6ec886bb2a501b0df3eec
PhattaraPCRU Nitro5 <st641102064111@office365.pcru.ac.th>
2023-01-18 02:23:43 +0700

    Init info activities.

---
+++ app/src/main/java/pcru/phattara/campson_project/info_royal_pratumnak.java
@@ -0,0 +1,14 @@
+package pcru.phattara.campson_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class info_royal_pratumnak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_royal_pratumnak);
+    }
+}
---
+++ app/src/main/java/pcru/phattara/campson_project/info_temple_sonkaew.java
@@ -0,0 +1,14 @@
+package pcru.phattara.campson_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class info_temple_sonkaew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_temple_sonkaew);
+    }
+}
