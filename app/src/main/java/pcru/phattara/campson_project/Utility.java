ba0ed83f3a6569dda84a865ab25a639dbf28d510
PhattaraPCRU Nitro5 <st641102064111@office365.pcru.ac.th>
2023-01-18 01:11:32 +0700

    Add intent for short use.

--- app/src/main/java/pcru/phattara/campson_project/Utility.java
+++ app/src/main/java/pcru/phattara/campson_project/Utility.java
@@ -1,8 +1,15 @@
 package pcru.phattara.campson_project;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class Utility {
     public static String get_ts() {
         long tsLong = System.currentTimeMillis()/1000;
         return Long.toString(tsLong);
+    }
+    public static void page(Context context, Class<?> target) {
+        Intent intent = new Intent(context, target);
+        context.startActivity(intent);
     }
 }
