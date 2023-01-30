38d4f34475d85a44f262f2208d51edd6771011b0
PhattaraPCRU Nitro5 <st641102064111@office365.pcru.ac.th>
2023-01-30 15:25:55 +0700

    Optimize variables and Lambda expression.

--- app/src/main/java/pcru/phattara/campson_project/HomeFragment.java
+++ app/src/main/java/pcru/phattara/campson_project/HomeFragment.java
@@ -1,10 +1,10 @@
 package pcru.phattara.campson_project;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,19 +17,14 @@ import com.smarteist.autoimageslider.SliderView;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class HomeFragment extends Fragment {
+    int[] temple_sonkaew = {R.drawable.temple_sonkaew1, R.drawable.temple_sonkaew2, R.drawable.temple_sonkaew3, R.drawable.temple_sonkaew4, R.drawable.temple_sonkaew5};
+    int[] waterfall_sridith = {R.drawable.waterfall_sridith1, R.drawable.waterfall_sridith2, R.drawable.waterfall_sridith3, R.drawable.waterfall_sridith4};
+    int[] royal_pratumnak = {R.drawable.royal_pratumnak1, R.drawable.royal_pratumnak2, R.drawable.royal_pratumnak3, R.drawable.royal_pratumnak4, R.drawable.royal_pratumnak5};
+    int[] jadee = {R.drawable.jadee1, R.drawable.jadee2, R.drawable.jadee3, R.drawable.jadee4, R.drawable.jadee5, R.drawable.jadee6, R.drawable.jadee7};
+    int[] khaokho_sacment = {R.drawable.khaokho_sacment1, R.drawable.khaokho_sacment2, R.drawable.khaokho_sacment3, R.drawable.khaokho_sacment4, R.drawable.khaokho_sacment5, R.drawable.khaokho_sacment6, R.drawable.khaokho_sacment7};
+    Context ThisActivity = getActivity();
     public HomeFragment() {
         // Required empty public constructor
-    }
-
-    int[] temple_sonkaew = {R.drawable.temple_sonkaew1,R.drawable.temple_sonkaew2,R.drawable.temple_sonkaew3,R.drawable.temple_sonkaew4,R.drawable.temple_sonkaew5};
-    int[] waterfall_sridith = {R.drawable.waterfall_sridith1,R.drawable.waterfall_sridith2,R.drawable.waterfall_sridith3,R.drawable.waterfall_sridith4};
-    int[] royal_pratumnak = {R.drawable.royal_pratumnak1,R.drawable.royal_pratumnak2,R.drawable.royal_pratumnak3,R.drawable.royal_pratumnak4,R.drawable.royal_pratumnak5};
-    int[] jadee = {R.drawable.jadee1,R.drawable.jadee2,R.drawable.jadee3,R.drawable.jadee4,R.drawable.jadee5,R.drawable.jadee6,R.drawable.jadee7};
-    int[] khaokho_sacment = {R.drawable.khaokho_sacment1,R.drawable.khaokho_sacment2,R.drawable.khaokho_sacment3,R.drawable.khaokho_sacment4,R.drawable.khaokho_sacment5,R.drawable.khaokho_sacment6,R.drawable.khaokho_sacment7};
-
-
-    public static HomeFragment newInstance() {
-        return new HomeFragment();
     }
 
     @Override
@@ -51,29 +46,46 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        SliderView sv1 = view.findViewById(R.id.imgS1);
-        SliderView sv2 = view.findViewById(R.id.imgS2);
-        SliderView sv3 = view.findViewById(R.id.imgS3);
-        SliderView sv4 = view.findViewById(R.id.imgS4);
-        SliderView sv5 = view.findViewById(R.id.imgS5);
-        SliderAdapter sa1 = new SliderAdapter(temple_sonkaew);
-        SliderAdapter sa2 = new SliderAdapter(waterfall_sridith);
-        SliderAdapter sa3 = new SliderAdapter(royal_pratumnak);
-        SliderAdapter sa4 = new SliderAdapter(jadee);
-        SliderAdapter sa5 = new SliderAdapter(khaokho_sacment);
-        SliderView[] sliderViews = {sv1,sv2,sv3,sv4,sv5};
-        SliderAdapter[] sliderAdapters = {sa1,sa2,sa3,sa4,sa5};
+//        SliderView sv1 = view.findViewById(R.id.imgS1);
+//        SliderView sv2 = view.findViewById(R.id.imgS2);
+//        SliderView sv3 = view.findViewById(R.id.imgS3);
+//        SliderView sv4 = view.findViewById(R.id.imgS4);
+//        SliderView sv5 = view.findViewById(R.id.imgS5);
+//        SliderAdapter sa1 = new SliderAdapter(temple_sonkaew);
+//        SliderAdapter sa2 = new SliderAdapter(waterfall_sridith);
+//        SliderAdapter sa3 = new SliderAdapter(royal_pratumnak);
+//        SliderAdapter sa4 = new SliderAdapter(jadee);
+//        SliderAdapter sa5 = new SliderAdapter(khaokho_sacment);
+//        SliderView[] sliderViews = {sv1,sv2,sv3,sv4,sv5};
+//        SliderAdapter[] sliderAdapters = {sa1,sa2,sa3,sa4,sa5};
+        SliderView[] sliderViews = {
+                view.findViewById(R.id.imgS1),
+                view.findViewById(R.id.imgS2),
+                view.findViewById(R.id.imgS3),
+                view.findViewById(R.id.imgS4),
+                view.findViewById(R.id.imgS5)
+        };
+        SliderAdapter[] sliderAdapters = {
+                new SliderAdapter(temple_sonkaew),
+                new SliderAdapter(waterfall_sridith),
+                new SliderAdapter(royal_pratumnak),
+                new SliderAdapter(jadee),
+                new SliderAdapter(khaokho_sacment)
+        };
         int randMin = 3000;
         int randMax = randMin + 6000;
         for (int i = 0; i < sliderViews.length; i++) {
-            sliderViews[i].setScrollTimeInMillis(ThreadLocalRandom.current().nextInt(randMin,randMax));
+            sliderViews[i].setScrollTimeInMillis(ThreadLocalRandom.current().nextInt(randMin, randMax));
             sliderViews[i].setSliderAdapter(sliderAdapters[i]);
             sliderViews[i].setIndicatorAnimation(IndicatorAnimationType.WORM);
             sliderViews[i].setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
             sliderViews[i].startAutoCycle();
         }
-        sv1.setOnClickListener(v -> Utility.page(getActivity(), info_temple_sonkaew.class));
-        TextView tv1 = view.findViewById(R.id.tv1);
-        tv1.setOnClickListener(v -> Utility.page(getActivity(),info_temple_sonkaew.class));
+        view.findViewById(R.id.tv1).setOnClickListener(v -> Utility.page(ThisActivity, info_temple_sonkaew.class));
+        view.findViewById(R.id.tv2).setOnClickListener(v -> Utility.page(ThisActivity, info_waterfall_sridith.class));
+        view.findViewById(R.id.tv3).setOnClickListener(v -> Utility.page(ThisActivity, info_royal_pratumnak.class));
+        view.findViewById(R.id.tv4).setOnClickListener(v -> Utility.page(ThisActivity, info_jadee.class));
+        view.findViewById(R.id.tv5).setOnClickListener(v -> Utility.page(ThisActivity, info_khaokho_sacment.class));
+
     }
 }
